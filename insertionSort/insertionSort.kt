f8db334c7fc8bb51b36a94e34e2c1bd7b16a1e1e
Tsutou <tsutoutakehara@gmail.com>
2018-10-16 02:16:11 +0900

    InsertionSort

---
+++ insertionSort/insertionSort.kt
@@ -0,0 +1,35 @@
+fun <T : Comparable<T>> insertionsort(items: MutableList<T>): List<T> {
+    if (items.isEmpty()) {
+        return items
+    }
+
+    for (count in 1..items.count() - 1) {
+        val item = items[count]
+        var i = count
+
+        while (i > 0 && item < items[i - 1]) {
+            items[i] = items[i - 1]
+            i -= 1
+        }
+        items[i] = item
+    }
+    return items
+}
+
+fun main(args: Array<String>) {
+    val names = mutableListOf(
+            "Stevie wonder",
+            "Alicia Keys",
+            "Eric Clapton",
+            "Aretha Flanklin",
+            "Metallica",
+            "Steely Dan",
+            "Xcape"
+    )
+
+    println(names)
+
+    var ordered = insertionsort(names)
+
+    println(ordered)
+}
